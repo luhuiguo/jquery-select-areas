b80f41226c3b783931136379fa19e4629e83aa0a
Malte Finsterwalder <malte.finsterwalder@red6-es.de>
2017-07-07 13:19:42 +0200

    Add an option to disable blurring

--- jquery.selectareas.js
+++ jquery.selectareas.js
@@ -573,7 +573,8 @@
                 overlayOpacity: 0.5,
                 areas: [],
                 onChanging: null,
-                onChanged: null
+                onChanged: null,
+                blurred: true
             };
 
         this.options = $.extend(defaultOptions, customOptions);
@@ -683,7 +684,7 @@
         this.$overlay.css({
             display : nbAreas? "block" : "none"
         });
-        if (nbAreas) {
+        if (nbAreas && this.options.blurred) {
             this.$image.addClass("blurred");
         } else {
             this.$image.removeClass("blurred");
